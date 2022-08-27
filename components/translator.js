90a6ec789d4bace01cf80ac8d4da6cc67a76e17a
kennethkamhw <kennethkamhw@gmail.com>
2022-08-28 01:48:03 +0800

    update

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,45 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+    constructor() {
+        this.locale = "american-to-british";
+    }
 
+    setLocale(locale) {
+        if (!locale) {
+            return { error: "No locale selected" };
+        }
+
+        if (locale === "american-to-british") {
+            this.locale = "american-to-british";
+        } else if (locale === "british-to-american") {
+            this.locale = "british-to-american";
+        } else {
+            return { error: "Invalid Locale" };
+        }
+    }
+
+    getLocale(locale) {
+        return this.locale;
+    }
+
+    checkTime(inputStr) {
+        const timeRegEx = /[0-9]{2}(.|:)[0-9]{2}/g;
+        let timeStr = inputStr.match(timeRegEx);
+        if (timeStr) {
+            let result = timeStr[0].match(/[0-9]{2}/g);
+            let hours = result[0];
+            let minutes = result[1];
+            if (this.locale === "american-to-british") {
+                return hours + "." + minutes;
+            } else {
+                return hours + ":" + minutes;
+            }
+        }
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,23 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
-module.exports = function (app) {
-  
-  const translator = new Translator();
+module.exports = function(app) {
+    const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+    app.route("/api/translate").post((req, res) => {
+        let { text, locale } = req.body;
+        let translation = `translation of ${text}`;
+
+        translator.setLocale(locale);
+        console.log(translator.getLocale);
+
+        translator.checkTime(text);
+
+        res.json({
+            text: text,
+            translation: translation,
+        });
+        console.log(`result = ${result}`);
     });
 };
