b5004cb588e5b6a0d9f65cbfa225a1fc9bac8208
Kenneth <kennethkamhw@gmail.com>
2022-08-31 16:26:19 +0000

    updated

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,96 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
+const ukToUs = 'british-to-american';
+const usToUk = 'american-to-british';
 
+class Translator {
+  constructor() {
+    this.text = "";
+    this.locale = ""
+  }
+
+  checkValid() {
+    // check if text is empty string
+    if (this.text==="") {
+      return { isValid: false, error: 'No text to translate' };
+    }
+
+    // check if locale is invalid value
+    if (!((this.locale===usToUk)|(this.locale===ukToUs))) {
+      return { isValid: false, error: 'Invalid value for locale field' };
+    }
+
+    // check if missing field(s)
+    if (!this.text | !this.locale) {
+      return { isValid: false, error: 'Required field(s) missing' };
+    }
+
+    // if all ok return true
+    return { isValid: true }
+  }
+
+  compareText(textToCompare) {
+    return this.text===textToCompare ? true: false;
+  }
+  
+  setText(text) {
+    this.text = text;
+    return this;
+  }
+
+  setLocale(input) {
+    this.locale = input;
+    return this;
+  }
+
+  convertTime() {
+    let regexUS = /(([0|1][0-9])|(2[0-4]))(\.)([0-5][0-9])/g;
+    let regexUK = /(([0|1][0-9])|(2[0-4]))(:)([0-5][0-9])/g;
+    let result = [];
+    let newStringList = [];
+    
+    if (this.locale === usToUk) {
+      result = this.text.match(regexUS);
+      if (result!==null) {
+        newStringList = result.map(e=> e.replace(".",":"));
+        result.forEach((e,i)=>{
+          this.text = this.text.replace(e, `<span class="highlight">${newStringList[i]}</span>`);
+      })
+    }
+    }else {
+      result = this.text.match(regexUK);
+      if (result!==null) { 
+        newStringList = result.map(e=> e.replace(":","."));
+        result.forEach((e,i)=>{
+          this.text = this.text.replace(e, `<span class="highlight">${newStringList[i]}</span>`);
+      });
+    }
+      }
+
+    return this;
+  }
+
+  translate() {
+    Object
+      .entries(americanOnly)
+      .forEach(([us, uk]) => {
+        this.text = this.text.replace(us, uk);
+      });
+    Object
+      .entries(americanToBritishSpelling)
+      .forEach(([us, uk]) => {
+        this.text = this.text.replace(us, uk);
+      });
+    Object
+      .entries(americanToBritishTitles)
+      .forEach(([us, uk]) => {
+        this.text = this.text.replace(us, uk);
+      });
+
+    
+    return this;
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,39 @@ module.exports = function (app) {
   
   const translator = new Translator();
 
+  app.use((req, res, next) => {
+    console.log(`Method = ${req.method}, Path = ${req.originalUrl}, Body = ${JSON.stringify(req.body)}`);
+    next();
+  })
+  
   app.route('/api/translate')
-    .post((req, res) => {
+    .post((req, res) => { 
+      translator
+        .setText(req.body.text)
+        .setLocale(req.body.locale);
+
+      if (!translator.checkValid().isValid) {
+        console.log(translator.checkValid())
+        res.json({ error: translator.checkValid().error });
+        return;
+      }
+      
+      let result = {
+        text: req.body.text,
+        translation: translator.convertTime().translate().text
+      };
+
+      if (translator.compareText(req.body.text)) {
+        res.json({
+          text: req.body.text,
+          translation: 'Everything looks good to me!'
+        });
+        return;
+      }
+
+      console.log(result);
+      
+      res.json(result);
       
     });
 };
